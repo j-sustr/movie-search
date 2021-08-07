490eb4bc008f6fb1af0c3e57b3cdaa40fa4b7645
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-08-07 22:54:40 +0200

    feat(server): setup dev cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
 import 'dotenv-safe/config';
 import express from 'express';
-import { PORT } from './config/config';
+import { PORT, __prod__ } from './config/config';
 import routes from './routes';
 import logger from './common/logger';
+import cors from 'cors';
 
 const app = express();
-
+if (!__prod__) {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
 app.use(routes);
 app
   .listen(PORT, () => logger.info(`server started on localhost:${PORT}`))
